bd7618956cf860ba19c8bb3a75efa87efad50a57
GSpy <gspy0231@gmail.com>
2023-11-29 17:48:16 +0900

    Feature : 생성자 및 Getter 추가

--- src/entity/Member.java
+++ src/entity/Member.java
@@ -6,4 +6,22 @@ public class Member {
     private String id;
     private String password;
     private List<Integer> bookList;
+
+    public Member(String id, String password, List<Integer> bookList) {
+        this.id = id;
+        this.password = password;
+        this.bookList = bookList;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<Integer> getBookList() {
+        return bookList;
+    }
 }
