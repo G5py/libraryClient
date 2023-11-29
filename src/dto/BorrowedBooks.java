1517f660a46cfefc9ca41d20e064607de98a5a08
GSpy <gspy0231@gmail.com>
2023-11-29 19:14:28 +0900

    Feature : BorrowedBooks DTO 추가

---
+++ src/dto/BorrowedBooks.java
@@ -0,0 +1,17 @@
+package dto;
+
+import entity.Book;
+
+import java.util.List;
+
+public class BorrowedBooks {
+    private List<Book> books;
+
+    public BorrowedBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+}
