a8b18ace36f27b7661f70fcea3945152d2fa2b5c
GSpy <gspy0231@gmail.com>
2023-11-30 16:08:58 +0900

    Feature : RepositoryManager 클래스 구현.
    Repository 클래스들의 싱글톤 유지를 위한 클래스.
    새로운 Repository 클래스 구현 시 수정 필요함.

--- src/repository/RepositoryManager.java
+++ src/repository/RepositoryManager.java
@@ -1,4 +1,20 @@
+// 작성자 : 김상규
+
 package repository;
 
+
+// Repository들의 싱글톤 유지를 담당함.
 public class RepositoryManager {
+    private static BookHashmapRepository   bookRepository   = new BookHashmapRepository();
+    private static MemberHashmapRepository memberRepository = new MemberHashmapRepository();
+
+
+    public static BookHashmapRepository getBookHashmapRepository() {
+        return bookRepository;
+    }
+
+    public static MemberHashmapRepository getMemberHashmapRepository() {
+        return memberRepository;
+    }
+
 }
