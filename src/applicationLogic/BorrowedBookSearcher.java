9d33070f7b28cc22cc16df1b3cd12f926808622b
GSpy <gspy0231@gmail.com>
2023-11-30 16:10:23 +0900

    Feature : BorrowedBookSearcher 클래스 구현.
    사용자가 빌린 책을 조회하는 기능을 담당하는 클래스.

--- src/applicationLogic/BorrowedBookSearcher.java
+++ src/applicationLogic/BorrowedBookSearcher.java
@@ -1,4 +1,34 @@
+// 작성자 : 김상규
+
 package applicationLogic;
 
+import dto.BorrowedBooks;
+import entity.Book;
+import repository.BookHashmapRepository;
+import repository.MemberHashmapRepository;
+import repository.RepositoryManager;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BorrowedBookSearcher {
+    private BookHashmapRepository   bookRepository;
+    private MemberHashmapRepository memberRepository;
+
+    public BorrowedBookSearcher() {
+        this.bookRepository   = RepositoryManager.getBookHashmapRepository();
+        this.memberRepository = RepositoryManager.getMemberHashmapRepository();
+    }
+
+    public BorrowedBooks search(Integer id) {
+        List<Book> books = memberRepository.findByKey(id)
+                .getBookList().stream()
+                .map(bookRepository::findByKey)
+                .collect(Collectors.toList());
+
+        return new BorrowedBooks(books);
+
+    }
+
+
 }
