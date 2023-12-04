76ba3aeecd05a3a848cabde29f33de1f33d276cf
yjpark0307 <yjpark0307@gmail.com>
2023-12-04 18:10:28 +0900

    LibraryCli [종료] 추가

--- src/boundary/LibraryCli.java
+++ src/boundary/LibraryCli.java
@@ -22,7 +22,7 @@ public class LibraryCli
     {
         while(true){
             System.out.println("------------------------------------------------------------");
-        System.out.println("원하시는 항목을 선택하십시오.\n[1] 도서검색\n[2] 도서대출\n[3] 도서반납\n[4] 빌린도서목록조회\n[5] 책 요청\n[6] 로그인\n[7] 회원가입");
+        System.out.println("원하시는 항목을 선택하십시오.\n[1] 도서검색\n[2] 도서대출\n[3] 도서반납\n[4] 빌린도서목록조회\n[5] 책 요청\n[6] 로그인\n[7] 회원가입\n[8] 종료");
         System.out.print("입력 : ");
         int choice = scanner.nextInt();
         switch (choice) {
@@ -47,6 +47,9 @@ public class LibraryCli
             case 7:
                 register();
                 break;
+            case 8:
+                System.out.println("시스템을 종료합니다.");
+                return;
             default:
                 System.out.println("잘못된 입력입니다.");
                 break;
@@ -150,16 +153,3 @@ public class LibraryCli
             }
         }
 }
-/*
-    int bookId;
-    String name, writer, publisher;
-    LibraryController.NonEditableModel model = new LibraryController.NonEditableModel(new Object[]{"고유번호", "도서명", "저자", "출판사"}, 0);
-    List<Book> bookList = searchedBooks.getBooks();
-        for (Book book : bookList) {
-                bookId = book.getNum();
-                name = book.getName();
-                writer = book.getWriter();
-                publisher = book.getPublisher();
-                String bookIdString = String.valueOf(bookId);
-                model.addRow(new Object[]{bookIdString, name, writer, publisher});
-                } */
