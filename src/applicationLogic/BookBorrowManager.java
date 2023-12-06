b0ccc8479bfe57dd7f59ea1aae56b81effb967e7
GoldenPigy <shrjsgnl1401@gmail.com>
2023-12-06 16:27:31 +0900

    대출기능 일차구현(추후수정)

--- src/applicationLogic/BookBorrowManager.java
+++ src/applicationLogic/BookBorrowManager.java
@@ -1,6 +1,42 @@
 package applicationLogic;
 
-public class BookBorrowManager
-{
+import entity.Book;
+import repository.BookHashmapRepository;
+import repository.MemberHashmapRepository;
+import repository.RepositoryManager;
+
+import java.util.List;
+
+public class BookBorrowManager {
+
+    private BookHashmapRepository bookRepository;
+    private MemberHashmapRepository memberRepository;
+
+    public BookBorrowManager() {
+        this.bookRepository   = RepositoryManager.getBookHashmapRepository();
+        this.memberRepository = RepositoryManager.getMemberHashmapRepository();
+    }
+
+    public String borrowBook(String bookName, String userID) {
+
+        List<Book> books = bookRepository.findByName(bookName);
+
+        if (books.isEmpty()) {
+            return "책을 찾을 수 없습니다.";
+        }
+
+        Book bookToBorrow = books.get(0);
+
+        if (!bookToBorrow.getDate().equals("")) {
+            return "해당 책은 이미 대출되어 있습니다.";
+        }
+
+        //책 상태 업데이트  날짜 사용하려면 import java.util.Date;사용 필요
+        bookToBorrow.setDate("20231206");
+        bookRepository.save(bookToBorrow.getNum(), bookToBorrow);
+        //memberRepository.save(userID, );
+        return "책 반납이 완료되었습니다.";
+    }
+
 
 }
