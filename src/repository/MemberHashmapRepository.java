45b057b1e407ef78ce39504721a24998739c556b
YeongJae Son <s27970@naver.com>
2023-12-08 13:46:52 +0900

    Refactor : 주석 제거

--- src/repository/MemberHashmapRepository.java
+++ src/repository/MemberHashmapRepository.java
@@ -11,7 +11,6 @@ public class MemberHashmapRepository extends HashmapRepository<String, Member> {
 
 
     public MemberHashmapRepository() {
-        //테스트용
         hashMap.put("bbb", new Member("bbb","1234",Collections.emptyList()));
     }
 
